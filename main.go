e537a72a7e7600675a38681fe0e34f6ce27a033d
gallifrey <956465331@qq.com>
2023-09-25 17:23:48 +0800

    fix: 补充数据库初始化

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "github.com/gallifreyCar/gallifrey-wechat/router"
+import (
+	"github.com/gallifreyCar/gallifrey-wechat/database"
+	"github.com/gallifreyCar/gallifrey-wechat/router"
+)
 
 func main() {
+	database.InitDB()
 	r := router.Router()
-
 	r.Run()
 }
