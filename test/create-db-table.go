affca58f599d714fdbd05d8e0ad6599a60790faf
gallifrey <956465331@qq.com>
2023-09-25 16:35:47 +0800

    feat: 添加慢查询日志记录

--- test/create-db-table.go
+++ test/create-db-table.go
@@ -6,14 +6,31 @@ import (
 	"github.com/gallifreyCar/gallifrey-wechat/utils"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
 	// 数据库配置
 	cfg := utils.ReadMySQLConfig("config")
 	fmt.Println(cfg)
+
+	// 日志配置
+	mysqlLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			//慢查询阈值
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		})
+
 	// 连接数据库
-	db, err := gorm.Open(mysql.Open(cfg), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(cfg), &gorm.Config{
+		Logger: mysqlLogger,
+	})
 	if err != nil {
 		panic("failed to connect database")
 	}
